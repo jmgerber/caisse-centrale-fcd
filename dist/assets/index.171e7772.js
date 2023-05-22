6df3134f86445d514b13532bfbe5f08359d0076c
jmgerber <jmgerber57@gmail.com>
2023-05-22 20:32:22 +0200

    Update dist

--- dist/assets/index.171e7772.js
+++ dist/assets/index.171e7772.js
@@ -147,8 +147,8 @@ Error generating stack: `+o.message+`
   align-items: center;
   & .numpad {
     display: grid;
-    grid-template-columns: repeat(3, clamp(44px, 3.8vw, 52px));
-    grid-template-rows: repeat(4, clamp(44px, 3.8vw, 52px));
+    grid-template-columns: repeat(3, clamp(40px, 3.8vw, 56px));
+    grid-template-rows: repeat(4, clamp(40px, 3.8vw, 56px));
     grid-gap: 0.4vw;
   }
   & button {
